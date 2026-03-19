036251e17fb0fe43dd13e48e71b5f9eaea44a283
shaiksameera-825 <shaiksameera825@gmail.com>
2026-03-19 18:12:14 +0530

    Initial commit

---
+++ Javascript/Day1.js
@@ -0,0 +1,31 @@
+// function check1()
+// {
+//     v1=document.getElementById("user").value
+//     alert(v1)
+// }
+// function check2(){
+//     v1=document.getElementById("user").value
+//     console.log(v1)
+// }
+// function check3(){
+//     v1=document.getElementById("user").value
+//     document.writeln(v1)
+// }
+//Day-3
+// function Sum()
+// {
+//  let a=Number(document.getElementById("a").value);
+//  let b=Number(document.getElementById("b").value);
+// let add= a+b;
+// document.getElementById("result").innerHTML="The sum is"  +add;
+// }
+// function Arrays(){
+//    let arr=[ 12,34,44,55,66];
+//  arr.push("ksrm");
+//  arr.unshift("sameera");
+//  arr.pop();
+//  arr.shift();
+//    arr.indexOf();
+//    arr.includes();
+//    document.writeln(arr.indexOf(44));
+// }
---
+++ Javascript/Day4.js
@@ -0,0 +1,39 @@
+// function Greater(){
+//    var marks=[10,20,30,40,50];
+//    let num=marks.map((a)=>a>30);
+//    document.writeln(num);
+// }
+// function dark() {
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = "white"; // text visible in dark mode
+// }
+
+// function light() {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
+// }
+//DAY-5
+// function test(){
+//     let paragraph=document.createElement("h1");
+//     paragraph.textContent="Hello world";
+//     document.getElementsByClassName("exam")[0].appendChild(paragraph);
+
+//     let header=document.createElement("h2");
+//     header.textContent="Hello";
+//     document.getElementsByClassName("exam")[1].appendChild(header);
+// }
+function a(){
+    alert("Javascript");
+}
+function c(){
+    if (confirm('Delete this item?')) {
+        alert("yes")
+} else {
+    alert("no")
+}
+}
+function p(){
+    var name = prompt('Enter your name:', 'Guest');
+    alert("hi"+name)
+
+}
